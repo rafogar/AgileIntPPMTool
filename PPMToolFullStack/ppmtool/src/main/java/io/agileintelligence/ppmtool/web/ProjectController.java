4b87478b72c45aec32ecd31b442183ca2bc9b297
Rafael Olivares <rafogar2@yahoo.com>
2023-09-28 00:42:05 -0600

    Validaciones @Valid, @BindingResult mensajes en JSON y depurados

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -3,6 +3,7 @@ package io.agileintelligence.ppmtool.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.agileintelligence.ppmtool.domain.Project;
 import io.agileintelligence.ppmtool.service.ProjectService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/project")
@@ -19,8 +21,13 @@ public class ProjectController {
 	private ProjectService projectService;
 	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+		
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
+		}
+		
 		Project project1 = projectService.saveOrUpdateProject(project);
-		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
 	}
 }
