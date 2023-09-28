69f1801b10275fe9a8c4a89aab70f2130cf7b34b
Rafael Olivares <rafogar2@yahoo.com>
2023-09-27 21:31:42 -0600

    Branch1 Project-Entity, Repositories-ProjectRepository

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,5 +1,46 @@
 package io.agileintelligence.ppmtool.domain;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;	
+	private String projectName;	
+	private String projectIdentifier;	
+	private String description;	
+	private Date start_date;	
+	private Date end_date;
+	
+	
+	private Date created_At;	
+	private Date updated_At;
+	
+	@PrePersist
+	protected void onCreate() {
+		this.created_At = new Date();
+		
+	}
+	
+	@PreUpdate
+	protected void onUpdated() {
+		this.updated_At = new Date();
+	}
+	
 
 }
---
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import io.agileintelligence.ppmtool.domain.Project;
+
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+	
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+	
+	
+}
