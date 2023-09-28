b79b0c8f1b4e1555d3879aad60639fc23b0319fb
Rafael Olivares <rafogar2@yahoo.com>
2023-09-28 00:13:50 -0600

    validaciones dentro del entity

--- PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -2,12 +2,17 @@ package io.agileintelligence.ppmtool.domain;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,15 +25,23 @@ public class Project {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;	
+	private Long id;
+	@NotBlank(message = "Project name is required")
 	private String projectName;	
+	@NotBlank(message = "Project Identifier is required")
+	@Size(min=4, max=5, message = "Please use 4 to 5 characters")
+	@Column(updatable = false, unique = true)
 	private String projectIdentifier;	
-	private String description;	
-	private Date start_date;	
-	private Date end_date;
+	@NotBlank(message = "Project description is required")
+	private String description;
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date end_date;	
 	
-	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date created_At;	
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 	
 	@PrePersist
